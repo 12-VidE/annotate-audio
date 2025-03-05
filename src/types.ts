72ab9a9025f8be464c7f1491ebe7d9f0189d7855
VidE <davide1202@live.it>
2025-03-05 13:32:11 +0100

    Init Release

---
+++ src/types.ts
@@ -0,0 +1,43 @@
+import { MarkdownPostProcessorContext, App } from "obsidian";
+
+export type AudioComment = {
+	time: number;
+	content: string;
+};
+
+/* export type AudioChunk = {
+	startTime: number;
+	endTime: number;
+}; */
+
+// What can be changed inside audio-box
+export type AudioBoxOptions = {
+	volume: number;
+	speed: number;
+	loop: boolean;
+	sticky: boolean;
+	title: string | undefined;
+	small: boolean;
+
+	/* chunk: AudioChunk | undefined; */
+};
+
+export const defaultAudioBoxOptions: AudioBoxOptions = {
+	volume: 0.5,
+	speed: 1,
+	loop: false,
+	sticky: false,
+	title: undefined,
+	small: false,
+
+	/* chunk: undefined, */
+};
+
+export type AudioBoxParameters = {
+	container: HTMLElement;
+	codeblockContent: string;
+	audioSource: string;
+	ctx: MarkdownPostProcessorContext;
+	player: HTMLAudioElement;
+	obsidianApp: App;
+};
